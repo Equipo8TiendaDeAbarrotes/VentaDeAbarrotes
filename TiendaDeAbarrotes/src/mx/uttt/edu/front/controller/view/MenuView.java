1afb67d6a5daac256ce813835f55318788eaa962
karlo <karlo@Karlos99>
2018-11-12 19:50:07 -0600

    Creacion de los respectivos paquetes del patron de diseño Front Controllers para el proyecto de Tienda de Abarrotes, con sus respectivas clases

---
+++ TiendaDeAbarrotes/src/mx/uttt/edu/front/controller/view/MenuView.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.uttt.edu.front.controller.view;
+
+/**
+ *
+ * @author karlo
+ */
+public class MenuView {
+    
+}
